d124826f223dbb5adaa5ea0cd5b7e9ac02bae7fe
Phong Huỳnh <p7157568@gmail.com>
2017-10-16 10:29:11 +0700

    feat - add library

---
+++ app/src/main/java/com/example/cpu11112_local/testleanbackshowcase/MainActivity.java
@@ -0,0 +1,10 @@
+package com.example.cpu11112_local.testleanbackshowcase;
+
+import android.app.Activity;
+
+/**
+ * Created by CPU11112-local on 10/16/2017.
+ */
+
+public class MainActivity extends Activity {
+}
---
+++ app/src/main/java/com/example/cpu11112_local/testleanbackshowcase/MainFragment.java
@@ -0,0 +1,8 @@
+package com.example.cpu11112_local.testleanbackshowcase;
+
+/**
+ * Created by CPU11112-local on 10/16/2017.
+ */
+
+public class MainFragment {
+}

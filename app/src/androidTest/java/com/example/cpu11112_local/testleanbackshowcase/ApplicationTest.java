99354486a738c2b49eac350ec1b21742e29df65a
Phong Huỳnh <p7157568@gmail.com>
2017-10-16 10:06:03 +0700

    Initial commit

---
+++ app/src/androidTest/java/com/example/cpu11112_local/testleanbackshowcase/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.cpu11112_local.testleanbackshowcase;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}

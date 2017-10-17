b3e77b329521419822c95866faebdac64dd6586b
Phong Huỳnh <p7157568@gmail.com>
2017-10-17 09:26:51 +0700

    feat - create dagger successful

--- app/src/main/java/com/example/cpu11112_local/testleanbackshowcase/MainFragment.java
+++ app/src/main/java/com/example/cpu11112_local/testleanbackshowcase/MainFragment.java
@@ -1,19 +1,10 @@
 package com.example.cpu11112_local.testleanbackshowcase;
 
-import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v17.leanback.app.BrowseFragment;
 import android.support.v17.leanback.widget.ArrayObjectAdapter;
-import android.support.v17.leanback.widget.ListRow;
 import android.support.v17.leanback.widget.PresenterSelector;
 
-import com.example.cpu11112_local.testleanbackshowcase.models.Card;
-import com.example.cpu11112_local.testleanbackshowcase.utils.Utils;
-import com.google.gson.Gson;
-
 import javax.inject.Inject;
-
-import dagger.android.AndroidInjection;
 
 /**
  * Created by CPU11112-local on 10/16/2017.
@@ -21,53 +12,69 @@ import dagger.android.AndroidInjection;
 
 public class MainFragment extends BrowseFragment implements MainView {
     @Inject
-    private ArrayObjectAdapter mRowsAdapter;
+    ArrayObjectAdapter mRowsAdapter;
 
     @Inject
-    private PresenterSelector presenterSelector;
+    PresenterSelector presenterSelector;
 
     public static MainFragment newInstance() {
         return new MainFragment();
     }
-
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        AndroidInjection.inject(this);
-        super.onActivityCreated(savedInstanceState);
-        setupUIElements();
-        setAdapter(mRowsAdapter);
-        createRows();
-        setupRowAdapter();
-        setupEventListeners();
-    }
-
-    private void setupUIElements() {
-        setTitle(getString(R.string.browse_title));
-        setBadgeDrawable(getResources().getDrawable(R.drawable.title_android_tv, null));
-        setHeadersState(HEADERS_DISABLED);
-        setHeadersTransitionOnBackEnabled(false);
-        setBrandColor(getResources().getColor(R.color.fastlane_background));
-    }
-
-    private void createRows() {
-        String json = Utils
-                .inputStreamToString(getResources().openRawResource(R.raw.launcher_cards));
-        CardRow[] rows = new Gson().fromJson(json, CardRow[].class);
-        for (CardRow row : rows) {
-            mRowsAdapter.add(createCardRow(row));
-        }
-    }
-
-    private ListRow createCardRow(CardRow cardRow) {
-        ArrayObjectAdapter listRowAdapter = new ArrayObjectAdapter(presenterSelector);
-        for (Card card : cardRow.getCards()) {
-            listRowAdapter.add(card);
-        }
-        return new ListRow(listRowAdapter);
-    }
-
-    private void setupEventListeners() {
-
-    }
-
+//
+//    @Override
+//    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+//        AndroidInjection.inject(this);
+//        super.onActivityCreated(savedInstanceState);
+//        setupUIElements();
+//        setAdapter(mRowsAdapter);
+//        createRows();
+//        setupEventListeners();
+//    }
+//
+//    private void setupUIElements() {
+//        setTitle(getString(R.string.browse_title));
+//        setBadgeDrawable(getResources().getDrawable(R.drawable.title_android_tv, null));
+//        setHeadersState(HEADERS_DISABLED);
+//        setHeadersTransitionOnBackEnabled(false);
+//        setBrandColor(getResources().getColor(R.color.fastlane_background));
+//    }
+//
+//    private void createRows() {
+//        String json = Utils
+//                .inputStreamToString(getResources().openRawResource(R.raw.launcher_cards));
+//        CardRow[] rows = new Gson().fromJson(json, CardRow[].class);
+//        for (CardRow row : rows) {
+//            mRowsAdapter.add(createCardRow(row));
+//        }
+//    }
+//
+//    private ListRow createCardRow(CardRow cardRow) {
+//        ArrayObjectAdapter listRowAdapter = new ArrayObjectAdapter(presenterSelector);
+//        for (Card card : cardRow.getCards()) {
+//            listRowAdapter.add(card);
+//        }
+//        return new ListRow(listRowAdapter);
+//    }
+//
+//    private void setupEventListeners() {
+//        setOnItemViewClickedListener(new ItemViewClickedListener());
+//        setOnItemViewSelectedListener(new ItemViewSelectedListener());
+//    }
+//
+//
+//    private final class ItemViewClickedListener implements OnItemViewClickedListener {
+//        @Override
+//        public void onItemClicked(Presenter.ViewHolder itemViewHolder, Object item,
+//                                  RowPresenter.ViewHolder rowViewHolder, Row row) {
+//            // TODO: 10/17/2017 implement this method
+//        }
+//    }
+//
+//    private final class ItemViewSelectedListener implements OnItemViewSelectedListener {
+//
+//        @Override
+//        public void onItemSelected(Presenter.ViewHolder itemViewHolder, Object item,
+//                                   RowPresenter.ViewHolder rowViewHolder, Row row) {
+//        }
+//    }
 }
---
+++ app/src/main/java/com/example/cpu11112_local/testleanbackshowcase/models/CardRow.java
@@ -0,0 +1,41 @@
+package com.example.cpu11112_local.testleanbackshowcase.models;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+/**
+ * Created by CPU11112-local on 10/17/2017.
+ */
+
+public class CardRow {
+    // default is a list of cards
+    public static final int TYPE_DEFAULT = 0;
+    // section header
+    public static final int TYPE_SECTION_HEADER = 1;
+    // divider
+    public static final int TYPE_DIVIDER = 2;
+
+    @SerializedName("type") private int mType = TYPE_DEFAULT;
+    // Used to determine whether the row shall use shadows when displaying its cards or not.
+    @SerializedName("shadow") private boolean mShadow = true;
+    @SerializedName("title") private String mTitle;
+    @SerializedName("cards") private List<Card> mCards;
+
+    public int getType() {
+        return mType;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public boolean useShadow() {
+        return mShadow;
+    }
+
+    public List<Card> getCards() {
+        return mCards;
+    }
+
+}
